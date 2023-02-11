4a7173ebca6252bf5e0f80f5fee94727409f2db4
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-11 02:34:47 -0500

    Fixed executor problem

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import time
 
 if __name__ == "__main__":
     all_results = []
-    try:
-        while True:
-            record(10)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        try:
+            while True:
+                record(10)
 
-            # print("Simulating recording...")
-            # time.sleep(5)
-        
-            with concurrent.futures.ThreadPoolExecutor() as executor:
+                # print("Simulating recording...")
+                # time.sleep(5)
+            
                 future = executor.submit(analyze)
-                all_results.append(future.result())
+                # all_results.append(future.result())
                 # print(future.result())
-    except KeyboardInterrupt:
-        all_results = '\n'.join(all_results)
-        print("Here's the summary of the whole thing: ")
-        summarize(all_results, 7)
+        except KeyboardInterrupt:
+            executor.shutdown(wait=False)
+            all_results = '\n'.join(all_results)
+            print("Here's the summary of the whole thing: ")
+            summarize(all_results, 7)
         
