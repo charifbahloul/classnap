009977d83e38be7b81a4ac54315b1fa58bd41074
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-11 00:54:19 -0500

    Added whisper

--- audioReciever.py
+++ audioReciever.py
@@ -1,4 +1,5 @@
 import pyaudio
+import whisper
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -6,25 +7,31 @@ CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
 
-p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+def record():
+    p = pyaudio.PyAudio()
 
-print("* recording")
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
-frames = []
+    frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
 
-print("* done recording")
+    print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
+    return frames
+
+def whisperStuff(frames):
+    model = whisper.load_model("base.en")
+    result = model.transcribe(frames, language = "english", verbose=True)
+    print(result)
