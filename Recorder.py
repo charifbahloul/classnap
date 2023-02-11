a759240b6c039221ca9d594640a5f844bde2abae
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-11 01:29:36 -0500

    Added whisper stuff

---
+++ Recorder.py
@@ -0,0 +1,34 @@
+import pyaudio, wave
+
+
+def record(FORMAT, CHANNELS, RATE, CHUNK, RECORD_SECONDS, WAVE_OUTPUT_FILENAME):
+    # create & configure microphone
+    mic = pyaudio.PyAudio()
+    stream = mic.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    # read & store microphone data per frame read
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    # kill the mic and recording
+    stream.stop_stream()
+    stream.close()
+    mic.terminate()
+
+    # combine & store all microphone data to output.wav file
+    outputFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    outputFile.setnchannels(CHANNELS)
+    outputFile.setsampwidth(mic.get_sample_size(FORMAT))
+    outputFile.setframerate(RATE)
+    outputFile.writeframes(b''.join(frames))
+    outputFile.close()
--- audioReciever.py
+++ audioReciever.py
@@ -1,37 +1,23 @@
 import pyaudio
 import whisper
+import numpy as np
+from Recorder import record
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+model = whisper.load_model("large_v2")
 
-
-def record():
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    return frames
-
-def whisperStuff(frames):
-    model = whisper.load_model("base.en")
-    result = model.transcribe(frames, language = "english", verbose=True)
+def whisperStuff():
+    # Read the audio data from the file into a numpy array
+    result = model.transcribe(WAVE_OUTPUT_FILENAME, language = "english")["text"]
     print(result)
+
+    return result
+
+if __name__ == "__main__":
+    record(FORMAT, CHANNELS, RATE, CHUNK, RECORD_SECONDS, WAVE_OUTPUT_FILENAME)
+    whisperStuff()
