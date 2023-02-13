590c1bd625cffd146544f8665409412caf1edfe6
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-13 01:37:01 -0500

    Fixed it so that it can be compiled

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import cx_Freeze
 import sys
 sys.setrecursionlimit(5000)
 
-executables = [cx_Freeze.Executable("app.py", base = "Win32gui", icon="files\Classnap.ico")]
+executables = [cx_Freeze.Executable("app.py", icon="files\Classnap.ico")]
 
 cx_Freeze.setup(
     name="Classnap",
-    # options={"build_exe": {"excludes": ['pygame._camera_opencv_highgui', 'pandas', 'scipy.special.tests', 'scipy.special._precompute', 'scipy.special', 'pyqt5', 'pyside2'], "includes":["plyer.platforms.win.notification", "bitly_api", 'adhanpy', 'zoneinfo', 'tzdata']}}, # Those cause recursion errors (trying to import too much to get there).
+    version= "0.1",
+    options={"build_exe": {"includes": ['torch', 'jinja2' , 'jinja2.ext'], "include_files": ['templates/index.html'], "excludes": ['Tkinter']}}, # Those cause recursion errors (trying to import too much to get there).
     executables=executables,
-    # Add torch as a package.
-    packages=["torch"],
     author_email="shariefbahloul@gmail.com",
-    description="Nap in class"
+    description="Nap in class",
 )
