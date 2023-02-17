680c6467908a0f830f7ea6f73cf95c0e9b3cb125
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-16 19:17:54 -0500

    Release version

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ sys.setrecursionlimit(5000)
 
 executables = [cx_Freeze.Executable("main.py")]
 
+# TYorch isn't included because it makes it too big.
+
 cx_Freeze.setup(
     name="Classnap",
-    version= "0.1",
-    options={},
+    version= "0.2",
+    options={"build_exe": {"excludes": ["torch"]}},
     executables=executables,
     author_email="shariefbahloul@gmail.com",
     description="Nap in class",
