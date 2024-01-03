54d5f8c5f3e1500021bf1546c139312fbc8e9018
Charif Bahloul <shariefbahloul@gmail.com>
2024-01-02 20:30:42 -0500

    Finally fixed setup. If it says importerror and that there isn't _wrapper, I finally figured out the solution is to copy into lib folder as stated in setup.py.

--- setup.py
+++ setup.py
@@ -4,16 +4,23 @@ sys.setrecursionlimit(5000)
 
 executables = [cx_Freeze.Executable("app.py")]
 
-# Torch isn't included because it makes it too big.
-
 cx_Freeze.setup(
     name="ClassNap",
-    version= "1.42",
-    options={"build_exe": {"excludes": ["torch"], "includes": ['jinja2' , 'jinja2.ext'], "include_files": ['templates/index.html']}},
+    version="1.42",
     executables=executables,
     author="Charif Bahloul",
     author_email="shariefbahloul@gmail.com",
     description="Nap in class",
+    options={
+        "build_exe": {
+            "include_files": ['templates/index.html'],
+            "bin_includes": [
+                ("C:/Users/shari/anaconda3/envs/ClassnapEnvironment/Lib/site-packages/curl_cffi.libs/libcurl-cbb416caa1dd01638554eab3f38d682d.dll"),
+            ],
+            "includes": ["curl_cffi"]
+        }
+    }
 )
 
 # To run, put the following in the terminal: python setup.py build
+# Make sure to copy over libcurl-cbb416caa1dd01638554eab3f38d682d.dll into lib folder. For some reason, g4f doesn't work without it.
